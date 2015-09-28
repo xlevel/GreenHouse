9793c3a1d89158fc7b1c5611dd11c37036cf008a
Jim Hurst <jim@xlevel.org.uk>
2015-09-28 21:16:02 +0000

    Enabled display of current reading to screen

--- gh.py
+++ gh.py
@@ -1,6 +1,7 @@
 import sensor
 import time
 import data
+import screen
 from config import Config
 
 config = Config()
@@ -13,5 +14,7 @@ while True:
 	print '{0} {1:0.1f}*C, {2:0.1f}%'.format(time.strftime('%Y-%m-%d %H:%M:%S', reading[0]), reading[1], reading[2])
 
 	data.WriteReading(reading[0], reading[1], reading[2])
+	
+	screen.Write(reading[0], reading[1], reading[2])
 
 	time.sleep(5)
--- screen.py
+++ screen.py
@@ -2,13 +2,14 @@
 from oled.device import ssd1306
 from oled.render import canvas
 from PIL import ImageFont
+import time
 
-def Write(time, temperature, humidity)
+def Write(dateTime, temp, humidity):
 	device = ssd1306(port=1, address=0x3C)
 
 	with canvas(device) as draw:
 		font = ImageFont.truetype("FreeSans.ttf", 12)
-		draw.text((10,5), "Time: 12:21:31", font=font, fill=255)
-		draw.text((10,25), "Temp: 19.5\xb0C", font=font, fill=255)
-		draw.text((10,45), "Humi: 50.0%", font=font, fill=255)
+		draw.text((10,5), "Time: {0}".format(time.strftime('%Y-%m-%d %H:%M:%S', dateTime)), font=font, fill=255)
+		draw.text((10,25), "Temp: {0:0.1f}\xb0C".format(temp), font=font, fill=255)
+		draw.text((10,45), "Humi: {0:0.1f}%".format(humidity), font=font, fill=255)
 
